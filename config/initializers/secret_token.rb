52e36ab78ef48bfc6a5e9d33dc2acff0602e458e
WHOMENTORSDOTCOM <Rauhmel@WHOmentors.com>
2014-03-11 10:06:36 -0700

    Reset Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo::Application.config.secret_key_base = 'aeabe3f4ffb098996b3e897283896635c7dc549e7c2646be933cde2b60b528304e0320b66a40632c1833ef19f83b69fe0eb365a0f4d6b1edf02b36703c0cb91c'
